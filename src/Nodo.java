3914e8da0174a37390bf24faf7c6311373f999dd
Miguel <miguelpa2454@gmail.com>
2017-11-21 09:49:01 -0600

    Subiendo proyecto

---
+++ src/Nodo.java
@@ -0,0 +1,36 @@
+/**
+ * Created by User on 15/11/2017.
+ */
+public class Nodo {
+    private int valor;
+    private Nodo proximo; //punter hacia el siguiente
+
+    //Constructor simple, inicializa sin valor
+    public Nodo(){
+        this.valor=0;
+        this.proximo=null;
+    }
+    //Constructor
+    public Nodo
+    (int valor){
+        this.valor=valor;
+        this.proximo=null;
+    }
+
+    public void setValor
+            (int valor){
+        this.valor = valor;
+    }
+
+    public void setProx(Nodo siguiente){
+        this.proximo = siguiente;
+    }
+
+    public int getValor(){
+        return this.valor;
+    }
+    public Nodo getProximo(){
+        return this.proximo;
+    }
+
+}
